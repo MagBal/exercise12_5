23517dc6faea8fdf6acbfcaa9b03a59b893ca693
Magda Balun <magi@op.pl>
2017-12-27 15:04:16 +0100

    Change .js file

--- scripts.js
+++ scripts.js
@@ -2,11 +2,10 @@ var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
+$.ajaxSetup({ cache: false });
+
 function getQuote() {
     $.getJSON(prefix + quoteUrl, createTweet);
-    $.ajaxSetup({
-        cache: false
-    });
 }
 
 function createTweet(input) {
@@ -34,7 +33,5 @@ function createTweet(input) {
 
 $(document).ready(function() {
     getQuote();
-    $('.trigger').click(function() {
-        getQuote();
-    })
+    $('.trigger').click(getQuote);
 });
